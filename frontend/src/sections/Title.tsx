6c58eaf73917456f101bc7e213c5e5460ede8c73
Jack-Lacy <jlacy@uwo.ca>
2023-11-09 00:43:52 -0500

    creating short intro about myself

--- frontend/src/sections/Title.tsx
+++ frontend/src/sections/Title.tsx
@@ -4,8 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
     
 function Title (){
     return <>
-        <h1>Title</h1>
-        <p>Introduction about myself, briefly explaining who I am, why I made this site, and what it shows off</p>
+        <h1>Welcome</h1>
+        <p>
+            Hello and welcome to my personal website! I am a software engineering student at the University of Western Ontario
+            currently on a year long internship before my 4th and final year of school. I am interning at CARFAX as a software
+            developer intern and have been enjoying my time learning how software engineers get things done in the real world.
+            I decided to build this website using some of the technologies I have learned in school and indstry to demonstrate
+            my skills and shed light on the experiences and projects I am the most proud of. Enjoy!
+        </p>
     </>
 }
 export default Title;
