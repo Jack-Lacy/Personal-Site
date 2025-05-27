b5c8f33aba1639045477e26314b0ebdabdb23c0e
Jack Lacy <jack.p.lacy@gmail.com>
2025-05-27 19:57:46 -0400

    got the slideshsow working

--- frontend/src/componants/Slideshow.tsx
+++ frontend/src/componants/Slideshow.tsx
@@ -1,9 +1,18 @@
+import Carousel from 'react-bootstrap/Carousel';
 import './Slideshow.scss'
 
 export default function Slideshow() {
     return (
         <div className="Slideshow">
-
+            <Carousel>
+                <Carousel.Item>
+                    <img src="/slideshow/lakelouise.jpg" alt="ski pic" />
+                    <Carousel.Caption>
+                        <h3>Lake Louise Ski Resort</h3>
+                        <p>Took this photo at the top of the mountain at Lake Louise when I went skiing with my family this year</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
     )
 }
