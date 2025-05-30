df5f6fc7056fb12270976be81acaa0a413e79445
Jack Lacy <jack.p.lacy@gmail.com>
2025-05-30 18:59:59 -0400

    Disabled overscroll, started project section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import About from "./sections/About";
+import Projects from "./sections/Projects";
 
 function App() {
-  return (
-    <div className="App">
-      <About/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <About/>
+            <Projects/>
+        </div>
+    );
 }
 
 export default App;
--- frontend/src/sections/Projects.tsx
+++ frontend/src/sections/Projects.tsx
@@ -1,5 +1,17 @@
+import "./Projects.scss"
+
 export default function Projects() {
     return (
-        <div className="Projects"></div>
+        <div className="Projects">
+            <div className="Text">
+                <h1>My Projects</h1>
+                <div className="Paragraph">
+                    <p>
+                        Here are some projects I've been working on. Some are school related and some are personal. I
+                        am very proud of my work and if I am able to, the Github repository will be provided.
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }
